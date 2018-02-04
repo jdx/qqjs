492331b2e0f002d8746f41f4a8674ca81f794138
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-02-03 18:07:39 -0800

    added a bunch of stuff

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 qqjs
 ====
 
-useful functions for writing node scripts
+A bunch of wrappers for various utilites. Ideal for writing shell scripts in node. Everything here runs in synchronous mode for ease of use.
 
 [![Version](https://img.shields.io/npm/v/qqjs.svg)](https://npmjs.org/package/qqjs)
 [![CircleCI](https://circleci.com/gh/jdxcode/qqjs/tree/master.svg?style=svg)](https://circleci.com/gh/jdxcode/qqjs/tree/master)
@@ -11,3 +11,71 @@ useful functions for writing node scripts
 [![Known Vulnerabilities](https://snyk.io/test/npm/qqjs/badge.svg)](https://snyk.io/test/npm/qqjs)
 [![Downloads/week](https://img.shields.io/npm/dw/qqjs.svg)](https://npmjs.org/package/qqjs)
 [![License](https://img.shields.io/npm/l/qqjs.svg)](https://github.com/jdxcode/qqjs/blob/master/package.json)
+
+Usage
+=====
+
+It's best to [look at the code](src/index.ts) to see what all is available, but here is an example of using it:
+
+```js
+const qq = require('qqjs')
+
+// silent mode, otherwise it logs out every command
+// qq.config.silent = true
+
+// run a command with qq.x this runs synchronously using execa
+// by default it will echo out to the screen the commmand, stdout/stderr and connect to stdin
+// can send either a string
+qq.x('git --version')
+
+// or specify the arguments
+qq.x('git' ['--version'])
+
+qq.cd('newdir')
+
+qq.cp('from', 'to')
+
+const pjson = qq.readJSON('package.json')
+qq.writeJSON('package.json', {})
+
+// for almost any command, if it takes a string you can also pass an array and it will automatically path.join()
+qq.writeJSON(['mydir', 'package.json'], {})
+```
+
+Status
+======
+
+- [x] x (exec)
+- [x] readJSON
+- [x] writeJSON
+- [x] path
+- [x] mkdirp
+- [x] env
+- [x] globby
+- [x] read file
+- [x] write file
+- [x] cd
+- [x] ls
+- [x] cp (use cpy)
+- [x] mv
+- [x] rm
+- [x] cwd
+- [x] file exists
+- [x] homedir
+- [ ] ln
+- [ ] is file/directory/symlink/etc
+- [ ] chmod
+- [ ] batch rename
+- [ ] sed
+- [ ] download files
+- [ ] upload files
+- [ ] aws s3
+- [ ] resolve-from
+- [ ] open-editor
+- [ ] hasha
+- [ ] temp files/dirs
+- [ ] git stuff?
+- [ ] find-up
+- [ ] read-pkg
+- [ ] which
+- [ ] pushd/popd
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as execa from 'execa'
 import * as fs from 'fs-extra'
+import * as Glob from 'glob'
+import * as _globby from 'globby'
 import * as loadJSONFile from 'load-json-file'
 import * as _ from 'lodash'
+import * as os from 'os'
 import * as path from 'path'
 import * as writeJSONFile from 'write-json-file'
 
@@ -43,17 +46,18 @@ export namespace x {
  * reads a json file in using load-json-file
  * this will automatically join the paths if you pass multiple strings with path.join()
  */
-export function readJSON(...filepaths: string[]) {
-  const filepath = path.join(...filepaths)
+export function readJSON(filepaths: string | string[]) {
+  const filepath = path.join(..._.castArray(filepaths))
   log('readJSON', filepath)
   return loadJSONFile.sync(filepath)
 }
 
 /**
  * writes a json file with write-json-file
- * this will automatically join the paths if you pass multiple strings with path.join()
+ * this will automatically join the paths if you pass an array of strings
  */
-export function writeJSON(filepath: string, data: any, options: writeJSONFile.Options = {}) {
+export function writeJSON(filepaths: string | string[], data: any, options: writeJSONFile.Options = {}) {
+  const filepath = path.join(..._.castArray(filepaths))
   log('writeJSON', filepath)
   writeJSONFile.sync(filepath, data, options)
 }
@@ -62,13 +66,113 @@ export function writeJSON(filepath: string, data: any, options: writeJSONFile.Op
  * creates a directory if it does not exist
  * this will automatically join the paths if you pass multiple strings with path.join()
  */
-export function mkdirp(...filepaths: string[]) {
-  const filepath = path.join(...filepaths)
-  log('mkdirp', filepath)
-  fs.mkdirp(filepath)
+export function mkdirp(...filepaths: (string | string[])[]) {
+  for (let fp of filepaths) {
+    const f = path.join(..._.castArray(fp))
+    log('mkdirp', f)
+    fs.mkdirpSync(f)
+  }
+}
+
+/**
+ * glob matcher (find files)
+ */
+export function globby(patterns: string | string[], options: Glob.IOptions = {}) {
+  log('globby', ...patterns)
+  return _globby.sync(patterns, options)
+}
+
+/**
+ * output string to file
+ * creates directory if not exists
+ */
+export function write(filepaths: string | string[], data: any, options = {}) {
+  const filepath = path.join(..._.castArray(filepaths))
+  log('write', filepath)
+  return fs.outputFileSync(filepath, data, options)
+}
+
+/**
+ * read file into string
+ */
+export function read(filepaths: string | string[], options = {}) {
+  const filepath = path.join(..._.castArray(filepaths))
+  log('read', filepath)
+  return fs.readFileSync(filepath, options)
+}
+
+/**
+ * cd into a directory
+ */
+export function cd(filepaths: string | string[]) {
+  const filepath = path.join(..._.castArray(filepaths))
+  log('cd', filepath)
+  return process.chdir(filepath)
+}
+
+/**
+ * list files in directory
+ */
+export function ls(filepaths: string | string[]) {
+  const filepath = path.join(..._.castArray(filepaths))
+  log('ls', filepath)
+  return fs.readdirSync(filepath)
+}
+
+/**
+ * copy files with fs.copy
+ * can copy directories
+ */
+export function cp(files: string | string[], destinationpaths: string | string[], options = {}) {
+  const destination = path.join(..._.castArray(destinationpaths))
+  for (let f of files) {
+    log('cp', f, destination)
+    fs.copySync(f, destination, options)
+  }
+}
+
+/**
+ * rm -rf
+ */
+export function rm(...filesArray: (string | string[])[]) {
+  for (let f of filesArray) {
+    f = path.join(..._.castArray(f))
+    log('rm', f)
+    fs.removeSync(f)
+  }
+}
+
+export function mv(source: string | string[], dest: string | string[]) {
+  source = path.join(..._.castArray(source))
+  dest = path.join(..._.castArray(dest))
+  log('mv', source, dest)
+  fs.moveSync(source, dest)
+}
+
+export function exists(filepath: string | string[]) {
+  filepath = path.join(..._.castArray(filepath))
+  const exists = fs.pathExistsSync(filepath)
+  log('exists', filepath, exists)
+  return exists
+}
+
+export function cwd() {
+  const cwd = process.cwd()
+  log('cwd', cwd)
+  return cwd
+}
+
+export function home() {
+  const home = os.homedir()
+  log('home', home)
+  return home
 }
 
 /**
  * we export path so you don't have to include it yourself
  */
 export {path}
+/**
+ * easy access to process.env
+ */
+export const env = process.env
