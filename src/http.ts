baf9d8c6d6411b6b6e54511c3b44d0048a51009e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-07 16:55:11 -0700

    create directory before downloading

--- src/http.ts
+++ src/http.ts
@@ -1,14 +1,14 @@
 import * as execa from 'execa'
+import * as fs from 'fs-extra'
 import * as _ from 'lodash'
 import * as path from 'path'
 
 import {x} from './exec'
 import {log} from './log'
 
-export * from './fs'
-
-export function download(url: string, filepath: string | string[]): Promise<execa.ExecaReturns> {
-  filepath = path.join(..._.castArray(filepath))
+export async function download(url: string, filepath?: string | string[]): Promise<execa.ExecaReturns> {
+  filepath = filepath ? path.join(..._.castArray(filepath)) : path.basename(url)
   log('download', url, filepath)
+  await fs.mkdirp(path.dirname(filepath))
   return x('curl', ['-fsSLo', filepath, url])
 }
